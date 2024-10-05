b7e6b107a2b3700c0575c08631f406e1071fe667
pariskang <1641866831@qq.com>
2024-10-06 00:05:52 +0800

    Update embedding_model.py

--- src/embedding_model.py
+++ src/embedding_model.py
@@ -1,7 +1,12 @@
-from sentence_transformers import SentenceTransformer
+# src/embedding_model.py
+
+from src.utils import create_custom_embedding_model
+import src.config as config
 
 def initialize_embedding_model(model_name):
-    # Initialize the embedding model
-    embedding_model = SentenceTransformer(model_name)
+    """
+    Initializes the embedding model.
+    """
+    embedding_model = create_custom_embedding_model(model_name)
     return embedding_model
         
