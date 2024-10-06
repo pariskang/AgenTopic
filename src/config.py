c979bdd607287655fd677072c071a59b53b17f3b
pariskang <1641866831@qq.com>
2024-10-06 23:56:47 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+# src/config.py
 
 import os
 from dotenv import load_dotenv
@@ -10,11 +11,13 @@ os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
 os.environ["OPENAI_API_BASE"] = "OPENAI_API_BASE"
 
 # Data file path
-DATA_FILE_PATH = 'data/psoriasis_papers_cleaned.xlsx'
+MAIN_DATA_FILE_PATH = 'data/psoriasis_papers_cleaned.xlsx'
+TEST_DATA_FILE_PATH = 'data/test_data.xlsx'
 FINAL_DATA_FILE = 'data_with_final_topics.xlsx'
+INITIAL_RESULTS_FILE = 'initial_topic_modeling_results.xlsx'
 
-# Embedding model name
-EMBEDDING_MODEL_NAME = 'sentence-transformers/all-MiniLM-L6-v2'
+# Embedding model name (use the Hugging Face model identifier)
+EMBEDDING_MODEL_NAME = 'distilbert-base-uncased'   #sentence-transformers/all-MiniLM-L6-v2
 
 # Number of iterations for refinement
 NUM_ITERATIONS = 3
@@ -23,4 +26,6 @@ NUM_ITERATIONS = 3
 GPT4_MODEL_NAME = 'gpt-4'
 GPT4_MINI_MODEL_NAME = 'gpt-4'
 CHATGPT_MODEL_NAME = 'gpt-4'
+        
+
         
